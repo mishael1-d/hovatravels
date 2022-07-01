a2b1f1f5af346a630e106ee8da3a11140265e44f
Mishael Dada <Mishaeldada@gmail.com>
2022-07-01 11:35:43 +0100

    fixed mask background

--- src/components/HeroSection/Hero.jsx
+++ src/components/HeroSection/Hero.jsx
@@ -4,12 +4,14 @@ import "./Hero.css";
 
 function Hero() {
   return (
+      <div className="mask-image">
     <div className="hero-wrapper">
-      <div className="bg-overlay">
-        <div className="hero-section__container container">
-          <h3 className="hero-title">Search and Book Flights and Hotels</h3>
-          <div className="tabpanel__container">
-            <BasicTabs />
+        <div className="bg-overlay">
+          <div className="hero-section__container container">
+            <h3 className="hero-title">Search and Book Flights and Hotels</h3>
+            <div className="tabpanel__container">
+              <BasicTabs />
+            </div>
           </div>
         </div>
       </div>
