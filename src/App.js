d9435df0d411cc87c0593cd7e4ffcd07a494a856
Mishael Dada <Mishaeldada@gmail.com>
2022-07-04 11:04:27 +0100

    added new images

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Navbar from './components/Navbar/Navbar';
 import Home from './pages/Home';
 
+export const AppContecxt = React. createContext()
 function App() {
+
+
+  const state = {
+
+  }
   return (
+    <AppContecxt.Provider value={state}>
     <Router>
       <Navbar/>
       <Routes>
         <Route path='/' element={<Home/>}/>
       </Routes>
     </Router>
+    </AppContecxt.Provider>
   );
 }
 
